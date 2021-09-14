f96043a88da44e48d1be3c59e8848e2c56501e73
LEVI770770 <82899765+LEVI770770@users.noreply.github.com>
2021-09-14 16:05:36 +0300

    Add files via upload

--- library.js
+++ library.js
@@ -1,77 +1,124 @@
-'use strict';
-
-const nconf = require.main.require('nconf');
+const simpleRecaptcha = require('simple-recaptcha-new');
+const meta = require.main.require('./src/meta');
 const winston = require.main.require('winston');
-const controllers = require('./lib/controllers');
+const emailer = require.main.require('./src/emailer');
+const helpers = require.main.require('./src/controllers/helpers');
 
-const plugin = {};
+const ContactPage = {
+    reCaptchaPubKey: null,
+    reCaptchaPrivKey: null,
+    contactEmail: null,
+    messageFooter: null,
+    // init the plugin
+    init(params, callback) {
+        let app = params.router;
+        let middleware = params.middleware;
 
-plugin.init = async (params) => {
-	const { router, middleware/* , controllers */ } = params;
-	const routeHelpers = require.main.require('./src/routes/helpers');
+        app.get('/contact', middleware.buildHeader, renderContact);
+        app.get('/api/contact', renderContact);
+        app.post('/contact', postContact);
 
-	/**
-	 * We create two routes for every view. One API call, and the actual route itself.
-	 * Use the `setupPageRoute` helper and NodeBB will take care of everything for you.
-	 *
-	 * Other helpers include `setupAdminPageRoute` and `setupAPIRoute`
-	 * */
-	routeHelpers.setupPageRoute(router, '/quickstart', middleware, [(req, res, next) => {
-		winston.info(`[plugins/quickstart] In middleware. This argument can be either a single middleware or an array of middlewares`);
-		setImmediate(next);
-	}], (req, res) => {
-		winston.info(`[plugins/quickstart] Navigated to ${nconf.get('relative_path')}/quickstart`);
-		res.render('quickstart', { uid: req.uid });
-	});
+        // admin panel
+        app.get('/admin/plugins/contact-page', middleware.admin.buildHeader, renderAdmin);
+        app.get('/api/admin/plugins/contact-page', renderAdmin);
 
-	routeHelpers.setupAdminPageRoute(router, '/admin/plugins/quickstart', middleware, [], controllers.renderAdminPage);
+        meta.settings.get('contactpage', (err, options) => {
+            if (err) {
+                winston.warn(`[plugin/contactpage] Unable to retrieve settings, will keep defaults: ${err.message}`);
+            }
+            else {
+                // load setting from config if exist
+                for(let settingName of ["reCaptchaPubKey", "reCaptchaPrivKey", "contactEmail", "messageFooter"]) {
+                    if (options.hasOwnProperty(settingName)) {
+                        ContactPage[settingName] = options[settingName];
+                    }
+                }
+            }
+        });
+
+        callback();
+    },
+    // add public token to api
+    getConfig(config, callback) {
+        config.contactpage = {
+            reCaptchaPubKey: ContactPage.reCaptchaPubKey
+        };
+        callback(null, config);
+    },
+    addToAdminNav(header, callback) {
+        header.plugins.push({
+            route: '/plugins/contact-page',
+            name: 'Contact page',
+        });
+
+        callback(null, header);
+    },
+    modifyEmail(mailData, callback) {
+        if(mailData && mailData.template == "contact-page") {
+            mailData = modifyFrom(mailData);
+        }  
+        callback(null, mailData);
+    }
 };
 
-/**
- * If you wish to add routes to NodeBB's RESTful API, listen to the `static:api.routes` hook.
- * Define your routes similarly to above, and allow core to handle the response via the
- * built-in helpers.formatApiResponse() method.
- *
- * In this example route, the `authenticate` middleware is added, which means a valid login
- * session or bearer token (which you can create via ACP > Settings > API Access) needs to be
- * passed in.
- *
- * To call this example route:
- *   curl -X GET \
- * 		http://example.org/api/v3/plugins/quickstart/test \
- * 		-H "Authorization: Bearer some_valid_bearer_token"
- *
- * Will yield the following response JSON:
- * 	{
- *		"status": {
- *			"code": "ok",
- *			"message": "OK"
- *		},
- *		"response": {
- *			"foobar": "test"
- *		}
- *	}
- */
-plugin.addRoutes = async ({ router, middleware, helpers }) => {
-	const middlewares = [
-		middleware.authenticateRequest,
-		// middleware.ensureLoggedIn,		// use this if you want only registered users to call this route
-	];
-	router.get('/quickstart/:param1', middlewares, (req, res) => {
-		helpers.formatApiResponse(200, res, {
-			foobar: req.params.param1,
-		});
-	});
-};
+function renderContact(req, res) {
+    return res.render('contact', {
+        recaptcha: ContactPage.reCaptchaPubKey,
+        breadcrumbs: helpers.buildBreadcrumbs([{ text: '[[contactpage:contact]]' }]),
+        title: "Contact"
+    });
+}
 
-plugin.addAdminNavigation = (header) => {
-	header.plugins.push({
-		route: '/plugins/quickstart',
-		icon: 'fa-tint',
-		name: 'Quickstart',
-	});
+function postContact(req, res) {
+    if(!req.body.email || !req.body.name || !req.body.subject || !req.body.message) {
+        return res.status(400).json({success: false, msg: '[[contactpage:error.incomplete]]'});
+    }
+    if(ContactPage.reCaptchaPubKey) {
+        if(!req.body['g-recaptcha-response']) {
+            return res.status(400).json({success: false, msg: '[[contactpage:error.incomplete.recaptcha]]'});
+        }
+        simpleRecaptcha(ContactPage.reCaptchaPrivKey, req.ip, req.body['g-recaptcha-response'], (err) => {
+            if (err) {
+                return res.status(400).json({success: false, msg: '[[contactpage:error.invalid.recaptcha]]'});
+            } else {
+                sendMail(req.body.email, req.body.name, req.body.subject, req.body.message, res);
+            }
+        });
+    } else {
+        sendMail(req.body.email, req.body.name, req.body.subject, req.body.message, res);
+    }
+}
 
-	return header;
-};
+function sendMail(replyTo, name, subject, message, res) {
+    let mailParams = {
+        content_text: message.replace(/(?:\r\n|\r|\n)/g, '<br>'),
+        footer_text: ContactPage.messageFooter,
+        from_name: name,
+        subject: subject,
+        template: 'contact-page',
+        uid: 0,
+        replyTo,
+    }
 
-module.exports = plugin;
+    mailParams = Object.assign({}, emailer._defaultPayload, mailParams);
+
+    emailer.sendToEmail('contact-page', ContactPage.contactEmail, undefined, mailParams, (error) => {
+        if (error) {
+            winston.error("[plugin/contactpage] Failed to send mail:" + error);
+            return res.status(500).json({success: false, message: '[[contactpage:error.mail]]'});
+        }
+        return res.json({success: true});
+    });
+}
+
+function modifyFrom(mailData) {
+    mailData.from_name = mailData._raw.from_name;
+    mailData.replyTo = mailData._raw.replyTo;
+    return mailData;
+}
+
+function renderAdmin(req, res) {
+    return res.render('admin/plugins/contact-page');
+}
+
+module.exports = ContactPage;
